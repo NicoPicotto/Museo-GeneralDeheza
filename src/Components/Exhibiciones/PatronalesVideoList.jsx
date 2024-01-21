ddfc58fe61f433f588e61a6d6cd345eb2ed0c86b
Nico Picotto <nicolaspicoto@Nicos-MacBook-Pro.local>
2024-01-21 12:24:03 -0300

    nuevos videos

--- src/Components/Exhibiciones/PatronalesVideoList.jsx
+++ src/Components/Exhibiciones/PatronalesVideoList.jsx
@@ -1,23 +1,15 @@
 const items = [
 	{
-		titulo: 'Entrevista Carlos Arreguez',
-		tag: 'Herencias',
-		url: 'https://www.youtube.com/embed/pn8Z-Nr1NVA?si=76mdto4UvfUT5qX7',
+		url: 'https://www.youtube.com/embed/XVNASZHPWgI?si=_oqRg7Zm1-k-XkpP',
 	},
 	{
-		titulo: 'Entrevista Hector Pastor',
-		tag: 'Herencias',
-		url: 'https://www.youtube.com/embed/lf3vXXtUfnU?si=9PPDQRjpOvx2_YZi',
+		url: 'https://www.youtube.com/embed/_5fb5fk712g?si=DuOQQiFmqc7dhpH4',
 	},
 	{
-		titulo: 'Entrevista Chamaco',
-		tag: 'Herencias',
-		url: 'https://www.youtube.com/embed/-oDTUQm9tMU?si=LKRCT4bLO4d7OYwa',
+		url: 'https://www.youtube.com/embed/ohesdc7a-mo?si=RWNVNAAif9OUrdXP',
 	},
 	{
-		titulo: 'Entrevista Ida Redelongui',
-		tag: 'Herencias',
-		url: 'https://www.youtube.com/embed/H8UrdH6c-Z4?si=RsZ9Z3Eoj4qMrDkZ',
+		url: 'https://www.youtube.com/embed/IRHCvj27M2c?si=yvSjz1BuSUcvD7uH',
 	},
 ];
 
