31124dedb376ff333357441d910914173f0bfa89
Nico Picotto <nicolaspicoto@Nicos-MacBook-Pro.local>
2024-01-30 11:32:51 -0300

    new features

--- src/Components/Exhibiciones/ExhibicionPatronales.jsx
+++ src/Components/Exhibiciones/ExhibicionPatronales.jsx
@@ -23,20 +23,15 @@ import { Mousewheel, Navigation } from 'swiper/modules';
 import data from './PatronalesSwiperList';
 import audios from './PatronalesAudioList';
 import AudioCard from './AudioPlayer';
-import SwiperExhibiciones from "./SwiperExhibiciones"
-import fotos from "./PatronalesFotosList"
+import SwiperExhibiciones from './SwiperExhibiciones';
+import fotos from './PatronalesFotosList';
 
 const ExhibicionPatronales = () => {
 	const [isMobile] = useMediaQuery('(max-width: 1100px)');
 
 	return (
-		<Stack
-			marginTop='-50px'
-			spacing={5}
-			
-		
-		>
-			<Grid templateColumns='repeat(2, 6fr)' gap={6} >
+		<Stack marginTop='-50px' spacing={5}>
+			<Grid templateColumns='repeat(2, 6fr)' gap={6}>
 				{items.map((item, index) => (
 					<GridItem
 						key={index}
@@ -84,7 +79,13 @@ const ExhibicionPatronales = () => {
 										<Heading as='h2' size='lg' color='terciario'>
 											{data.title}
 										</Heading>
-										<Text fontSize={data.title === "Juegos populares" ? "15px" : "lg"}>{data.content}</Text>
+										<Text
+											fontSize={
+												data.title === 'Juegos populares' ? '15px' : 'lg'
+											}
+										>
+											{data.content}
+										</Text>
 									</Stack>
 									<Image src={data.image} />
 								</Stack>
@@ -112,6 +113,13 @@ const ExhibicionPatronales = () => {
 				))}
 			</Grid>
 			<Divider borderColor='cuarto' marginBlock='2em' />
+			<Heading size='md' color='primario' mb='1em' textAlign='center'>
+				El 15 de Agosto de 2023, nos acompañaron los integrantes del grupo de
+				teatro Deja vú dándole vida a los juegos que se encontraban en
+				exposición. Con el objetivo de rememorar los anteriores festejos
+				patronales, se encontraban los famosos “Caballitos”, “La Ruleta” y “La
+				Chica y La Grande”.
+			</Heading>
 			<Stack>
 				<SwiperExhibiciones fotos={fotos} />
 			</Stack>
